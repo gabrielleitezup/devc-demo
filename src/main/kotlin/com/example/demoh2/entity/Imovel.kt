f49b0b7e7575cef44ab620e746a9a76dccec19eb
gabrielleitezup <gabriel.leite@zup.com.br>
2020-01-28 18:03:11 -0300

    Done

--- src/main/kotlin/com/example/demoh2/entity/Imovel.kt
+++ src/main/kotlin/com/example/demoh2/entity/Imovel.kt
@@ -1,10 +1,29 @@
 package com.example.demoh2.entity
 
+import org.hibernate.annotations.GenericGenerator
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.validation.constraints.NotNull
+
+@Entity
 data class Imovel(
+
+        @Id
+        @GeneratedValue(generator = "uuid")
+        @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
         var id: String? = null,
+
+        @NotNull
         var cep: String? = null,
+
+        @NotNull
         var endereco: String? = null,
+
+        @NotNull
         var numero: Int? = null,
+        @NotNull
         var tipoImovel: TipoImovel? = null,
+
         var andar: Int? = null
 )
--- src/main/kotlin/com/example/demoh2/repository/ImovelRepository.kt
+++ src/main/kotlin/com/example/demoh2/repository/ImovelRepository.kt
@@ -2,6 +2,7 @@ package com.example.demoh2.repository
 
 import com.example.demoh2.entity.Imovel
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-interface ImovelRepository : JpaRepository<Imovel, String> {
-}
+@RepositoryRestResource(collectionResourceRel = "imoveis", path = "imoveis")
+interface ImovelRepository : JpaRepository<Imovel, String>
